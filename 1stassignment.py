b6a73886987844d72a17224dd24857b02e4f9a28
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-05-22 19:45:08 +0530

    Update 1stassignment.py

--- 1stassignment.py
+++ 1stassignment.py
@@ -1,1 +1,10 @@
-print("helloworld")
+Num1 = int(input('Enter the First Number : ' ))
+Num2 = int(input('Enter the Second Number : ' ))
+Num3 = int(input('Enter the Third Number : ' ))
+
+if Num1>Num2 and Num1>Num3:
+    print('The First number is greater than other numbers')
+elif Num2>Num3 and Num2>Num1:
+    print('The Second number is greater than other numbers')
+else :
+    print('The Third number is greater than other numbers')
