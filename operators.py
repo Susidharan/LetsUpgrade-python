6e14f5051ae7dea1bfc25d7b75a7fe04d78f0880
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-05-23 12:05:04 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,17 @@
+num1= 4
+num2= 8
+
+Add = num1 + num2
+print('Addition of number 1 and 2 is : ', Add )
+
+Sub = num1-num2
+print('Subtraction number 1 and 2 is : ', Sub )
+
+multiply = num1*num2
+print('Multiplication ofr number 1 and 2 : ', multiply )
+
+div = num1/num2
+print('Division of number 1 and 2 is : ', div)
+
+mod = num1%num2
+print('Modulus od number 1 and 2 is : ', mod)
