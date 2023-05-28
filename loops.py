9fcf13d3a06c94a464b3bd054b4d58e71c763094
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-05-28 12:24:00 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      dhara
+#
+# Created:     23-05-2023
+# Copyright:   (c) dhara 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+## let print the odd number within 100
+
+num = 100
+A = 1
+i = 1
+while i<num:
+    if A%2!=0:
+     print(A)
+     A+=1
+     i+=1
+    elif A%2==0:
+        A+=1
+        i+=1
+    else :
+        break
+
+
+
+
+
+
