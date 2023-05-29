9e242f3898afaf4f9f55543920a23d0ce646dd0a
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-05-29 17:12:33 +0530

    Add files via upload

---
+++ operator and loop.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      dhara
+#
+# Created:     29-05-2023
+# Copyright:   (c) dhara 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+## program to print all numbers divisible by 5 within  100.
+
+n = 100
+i=1
+a = 5
+
+while i<=n:
+    if i%5==0:
+     print(i)
+     i+=1
+    else:
+        i+=1
