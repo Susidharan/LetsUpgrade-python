ad0409cc4e0c9e68d35dfd6e6605ca2a3637ca2d
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-06-01 19:58:52 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      dhara
+#
+# Created:     29-05-2023
+# Copyright:   (c) dhara 2023
+# Licence:     <your licence>
+#--------------------------------------------------------
+## Sum of the entered digits.
+n = input('Enter the two digit number : ')
+n1 = n[0]
+n2 = n[1]
+
+r = int(n1) + int(n2)
+print(r)
+
+
+
+
