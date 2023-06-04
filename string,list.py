8f42fc83370df1ac604ead42cf16e52c503ead74
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-06-04 11:34:46 +0530

    Add files via upload

---
+++ string,list.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      dhara
+#
+# Created:     04-06-2023
+# Copyright:   (c) dhara 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+##split the string
+s = 'program to practise string and list'
+words = s.split()
+print(words)
+
+##index of the words in the list
+s = list(range(10))
+print(s)
+
+##reverse the words in ther list
+w='ram seetha lakshman hanuman'
+words = w.split()
+for word in words:
+    print(word[::-1])
