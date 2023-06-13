16c95b0383ca6e3a4944e3dd833013eecb7eec61
Susidharan <70463708+Susidharan@users.noreply.github.com>
2023-06-13 10:48:47 +0530

    Add files via upload

---
+++ shutil assignment.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      dhara
+#
+# Created:     13-06-2023
+# Copyright:   (c) dhara 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import shutil
+
+source = r'\Users\dhara\Downloads\string.py'
+destination = r'\Users\dhara\Documents\string.py'
+
+shutil.copy(source, destination)
